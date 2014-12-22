286308263de65e669c4cb85471dcb6f575ba4548
gamwang <jonbai23@gmail.com>
2014-12-22 10:08:48 -0800

    Defensive Code

--- geoLocation/geoCoding.js
+++ geoLocation/geoCoding.js
@@ -2,7 +2,7 @@ var request = require('request');
 var fs = require('fs');
 
 // Put your api key in "apiKey" file
-var apiKey = fs.readFileSync('./apiKey');
+var apiKey = fs.readFileSync(__dirname + '/apiKey');
 
 module.exports = {
   doReverseGeo: function (lat, lng, cb) {
