f94b14b41feb7dbde1f7f308a2be4f3a2a93f98b
gamwang <jonbai23@gmail.com>
2014-12-22 07:02:08 -0500

    Added starting files

---
+++ web.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var path = require('path');
+
+var handler = require('./handler.js');
+
+var app = express();
+var http = require('http').Server(app);
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'jade');
+
+// REST API
+app.get('/', handler.index);
+
+// Server
+var server = http.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
+
