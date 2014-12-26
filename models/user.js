2f73d6ea22b5a1bd8e9d9773016853eecfb74e0a
deadmau <kithwon@naver.com>
2014-12-26 04:55:36 -0800

    Delete Twitter, Google+

--- models/user.js
+++ models/user.js
@@ -6,29 +6,16 @@ var bcrypt   = require('bcrypt-nodejs');
 //TODO: Add more on current schema
 var schema = mongoose.Schema({
 
-    local            : {
-        email        : String,
-        password     : String,
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
-
+  local      : {
+    email    : String,
+    password : String,
+  },
+  facebook   : {
+    id       : String,
+    token    : String,
+    email    : String,
+    name     : String
+  }
 });
 
 // generating a hash
