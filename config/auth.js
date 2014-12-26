7eebae0e739e7b2ba82d59859a4beaad6ad301d9
deadmau <kithwon@naver.com>
2014-12-26 04:52:53 -0800

    Delete Twitter, Google+

--- config/auth.js
+++ config/auth.js
@@ -6,15 +6,5 @@ module.exports = {
     'clientID' : 'your secret client id', // your App ID
     'clientSecret' : 'your secret id', // your App Secret
     'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
-  },
-  'twitterAuth' : {
-    'consumerKey' : 'your secret client id',
-    'consumerSecret' : 'your secret id ',
-    'callbackURL' : 'http://localhost:3000/auth/twitter/callback'
-  },
-  'googleAuth' : {
-    'clientID' : 'your secret client id',
-    'clientSecret' : 'your secret id ',
-    'callbackURL' : 'http://localhost:3000/auth/google/callback'
   }
 };
