0b8539cd5519665daa9c47e72741d45376d4f00d
deadmau <kithwon@naver.com>
2014-12-29 23:42:32 +0900

    remove fb keys

--- config/auth.js
+++ config/auth.js
@@ -3,8 +3,8 @@
 //TODO: Fill in id and secret keys for each SNS
 module.exports = {
   'facebookAuth' : {
-    'clientID' : '1548655275391138', // your App ID
-    'clientSecret' : 'c0c7c49cdfcdea6d69c9e8f759b92f4c', // your App Secret
+    'clientID' : '', // your App ID
+    'clientSecret' : '', // your App Secret
     'callbackURL' : 'http://pursuealong.com/profile'
   }
 };
