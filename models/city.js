b387669a7860b7aa5f53926dfb4f94822f8a2d7b
gamwang <jonbai23@gmail.com>
2015-01-08 17:03:26 +0800

    Added getGroup and getGroups for city model

--- models/city.js
+++ models/city.js
@@ -2,6 +2,7 @@
 
 var mongoose = require('mongoose');
 var _ = require('underscore');
+var Group = require('../models/group);
 
 var schmea = mongoose.Schema({
   city          : String,
@@ -20,5 +21,22 @@ schema.methods.addGroup = function(gid) {
   groups.push(gid);
 }
 
+/** Gets a group with a specific gid */
+schema.methods.getGroup = function(gid, cb) {
+  Group.findOne({'_id': gid}, function (err, group) {
+    cb(group);
+  });
+}
+
+/** Gets all the group in the city */
+schema.methods.getGroups: function(cb) {
+  var query = {};
+  this.groups.forEach(function(elem, index) {
+    query['_id'] = elem;
+  });
+  Group.find(query, function(groups) {
+    cb(groups);
+  });
+}
 module.exports = mongoose.model("City", schema);
 
