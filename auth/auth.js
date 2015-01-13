eb4a78f2066f11caae192d009addd80d4778eeec
deadmau <kithwon@naver.com>
2015-01-14 02:13:43 +0900

    fb key

--- auth/auth.js
+++ auth/auth.js
@@ -3,8 +3,8 @@
 //TODO: Fill in id and secret keys for each SNS
 module.exports = {
   'facebookAuth' : {
-    'clientID' : '1548655275391138', // your App ID
-    'clientSecret' : 'c0c7c49cdfcdea6d69c9e8f759b92f4c', // your App Secret
+    'clientID' : '', // your App ID
+    'clientSecret' : '', // your App Secret
     'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
   }
 };
