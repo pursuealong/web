5922f45fe3c5e8308d97ca6a94f2289441220df6
deadmau <kithwon@naver.com>
2015-01-14 02:12:19 +0900

    fixed login & signup page

--- controller/local_account_controller.js
+++ controller/local_account_controller.js
@@ -1,29 +1,12 @@
 module.exports = function(app, passport) {
 
-  // login
-  // login form
-  app.get('/login', function(req, res) {
-    res.render('user_login.ejs', { message: req.flash('loginMessage') });
-  });
-
   // process the login form
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/login', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-  }));
-
-  // authorizing login
-  // local login
-  app.get('/connect/local', function(req, res) {
-    res.render('connect-local.ejs', { message: req.flash('loginMessage') });
-  });
-
-  app.post('/connect/local', passport.authenticate('local-signup', {
-    successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-  }));
+  app.post('/login', 
+    passport.authenticate('local-login', {
+      successRedirect : '/', // redirect to the secure profile section
+      failureRedirect : '/', // redirect back to the signup page if there is an error
+      failureFlash : true // allow flash messages
+    }));
 
   // unlink account
   /* used to unlink accounts. for social accounts, just remove the token
@@ -33,7 +16,7 @@ module.exports = function(app, passport) {
     user.local.email    = undefined;
     user.local.password = undefined;
     user.save(function(err) {
-      res.redirect('/profile');
+      res.redirect('/');
     });
   });
 
--- controller/register_controller.js
+++ controller/register_controller.js
@@ -1,15 +1,9 @@
 module.exports = function(app, passport) {
 
-  // registration
-  // show the signup form
-  app.get('/register', function(req, res) {
-    res.render('user_registration.ejs', { message: req.flash('loginMessage') });
-  });
-
   // process the signup form
   app.post('/register', passport.authenticate('local-signup', {
-    successRedirect : '/profile', // redirect to the secure profile section
-    failureRedirect : '/register', // redirect back to the signup page if there is an error
+    successRedirect : '/', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
 
